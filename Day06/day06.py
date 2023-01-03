0435aa4a22d51040255f52423fcf6fe0773066fa
sudoyum <sudoyum95@protonmail.com>
2023-01-03 15:02:01 -0500

    Day 6 in python

---
+++ Day06/day06.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def solve_p1(input_data):
+    characters_processed = 0
+
+    for i in range(0, len(input_data[0]) - 3):
+        if len(set(input_data[0][i : i + 4])) == 4:
+            break
+        characters_processed += 1
+
+    print(f"PART1: {characters_processed + 4}")
+
+
+def solve_p2(input_data):
+    characters_processed = 0
+
+    for i in range(0, len(input_data[0]) - 13):
+        if len(set(input_data[0][i : i + 14])) == 14:
+            break
+        characters_processed += 1
+
+    print(f"PART2: {characters_processed + 14}")
+
+
+def run_solutions(input_file, part):
+    with open(input_file, "r", encoding="utf-8") as file_handle:
+        input_data = file_handle.read().splitlines()
+
+    if part == "p1":
+        solve_p1(input_data)
+    elif part == "p2":
+        solve_p2(input_data)
+    elif part == "both":
+        solve_p1(input_data)
+        solve_p2(input_data)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", help="file containing puzzle input", type=str)
+    parser.add_argument("part", help="", type=str, choices=["p1", "p2", "both"])
+
+    args = parser.parse_args()
+
+    try:
+        run_solutions(args.input_file, args.part)
+    except IOError as io_exception:
+        print(f"Opening file error: {io_exception.strerror} - {args.input_file}")
+
+
+if __name__ == "__main__":
+    main()
